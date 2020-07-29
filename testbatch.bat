0f375727f1613234312ce4c3cd38f0b8f4447eb5
teja3478 <51173870+teja3478@users.noreply.github.com>
2020-07-29 19:11:52 +0530

    commit

---
+++ testbatch.bat
@@ -0,0 +1,7 @@
+
+
+Powershell -ExecutionPolicy ByPass -File C:\psexec\Generate_Report2.ps1  D:\logs\20200721\8a88524aee5d4988a15189eaec8b78c1\launch\8a88524aee5d4988a15189eaec8b78c1.docx D:\logs\20200721\8a88524aee5d4988a15189eaec8b78c1\launch
+
+
+
+pause
